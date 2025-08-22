20b9d78e7ed70257381f12639a1d1c0e088f5e7c
SophyLatifah <sophy.latifah.22@gmail.com>
2025-08-22 09:02:55 +0700

    awal

---
+++ backend-siswa/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+// bikin koneksi pool (lebih aman dipakai buat web app)
+const db = mysql.createPool({
+  host: 'localhost',                // ganti sesuai server DB kamu
+  user: 'root',                     // username MySQL kamu
+  password: 'Sophybisa1!',          // password MySQL
+  database: 'siswa'                 // nama database
+});
+
+// cek koneksi
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Database connection failed: ', err.message);
+  } else {
+    console.log('Database connected!');
+    connection.release();
+  }
+});
+
+module.exports = db;
